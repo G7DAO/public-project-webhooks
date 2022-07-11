f57b7cafb45608edaaa486635ae68c8ab864d9a8
BlockSecNerd <blocknerd@protonmail.com>
2022-07-11 16:40:11 -0700

    0.0.2 - update api index

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
+import aid from './_api/api.aid';
+import bounty from './_api/api.bounty';
+import talent from './_api/api.talent';
 import waitlist from './_api/api.waitlist';
 import user from './_api/api.users';
 
+export const _aid = aid;
+export const _bounty = bounty;
+export const _talent = talent;
 export const _waitlist = waitlist;
 export const _user = user;
 
 const models = {
+  _aid,
+  _bounty,
+  _talent,
   _waitlist,
   _user,
 };
