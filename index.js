296213b496f87ec0b0ee83099934626674795e40
BlockSecNerd <blocknerd@protonmail.com>
2022-07-30 14:32:06 -0700

    0.1.9 add grant api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import aid from './_api/api.aid';
 import auth from './_auth';
 import bounty from './_api/api.bounty';
+import grant from './_api/api.grant';
 import talent from './_api/api.talent';
 import waitlist from './_api/api.waitlist';
 import user from './_api/api.users';
@@ -8,6 +9,7 @@ import user from './_api/api.users';
 export const _aid = aid;
 export const _auth = auth;
 export const _bounty = bounty;
+export const _grant = grant;
 export const _talent = talent;
 export const _waitlist = waitlist;
 export const _user = user;
@@ -16,6 +18,7 @@ const models = {
   _aid,
   _auth,
   _bounty,
+  _grant,
   _talent,
   _waitlist,
   _user,
