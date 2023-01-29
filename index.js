23b7c461352a876fc173fc11682943e3706ea7f5
blocksecnerd <blocknerd@protonmail.com>
2023-01-29 12:48:50 -0800

    create file management api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import aid from './_api/api.aid';
 import auth from './_auth';
 import bounty from './_api/api.bounty';
+import file from './_api/api.file';
 import grant from './_api/api.grant';
 import newsletter from './_api/api.newsletter';
 import talent from './_api/api.talent';
@@ -11,6 +12,7 @@ import xps from './_api/api.xps';
 export const _aid = aid;
 export const _auth = auth;
 export const _bounty = bounty;
+export const _file = file;
 export const _grant = grant;
 export const _newsletter = newsletter;
 export const _talent = talent;
@@ -22,6 +24,7 @@ const models = {
   _aid,
   _auth,
   _bounty,
+  _file,
   _grant,
   _newsletter,
   _talent,
